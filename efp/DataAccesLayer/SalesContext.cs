19ccd1c6f9cd68171563fb23c704af6ed3d8f2f5
Recep Pazarlı <receppazarli99@hotmail.com>
2024-09-21 13:00:37 +0300

    .

---
+++ efp/DataAccesLayer/SalesContext.cs
@@ -0,0 +1,26 @@
+﻿using efp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace efp.DataAccesLayer
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext() : base("SalesDatabase") { }
+
+        public DbSet<Product> Products { get; set; }
+
+
+        // Türkçe isimlendirme yaparsak Tablolarda çoğul eklerde s ies takıları sırasında Türkçe olacağı sorun çıkmaması için
+        // aşağıdaki methodu override ederek oluşturulan Türkçe tabloların sonuna "s" takısı gelmiyor.
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        //}
+
+    }
+}
